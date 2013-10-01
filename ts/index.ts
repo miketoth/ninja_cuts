653be24aa810e78308d6598b49009791a93551e2
Michael Toth <tofudrummer@gmail.com>
2013-10-01 16:26:12 -0400

    Changed index.html to index.php

--- ts/index.ts
+++ ts/index.ts
@@ -30,42 +30,46 @@ class Lesson {
 //////////////////////////////////////////////////
 
 $(document).ready(function() {
-
+	
 	// let user select which lesson to load
+	// display each lesson in the lesson folder
+	$("a").click(function() {
 
-	// get the specified JSON file and load it into the lesson 
-	$.getScript("lessons/test.js",function(data) {
-		var json_data = JSON.parse(data);
+		console.log(this.id);
+		// get the specified JSON file and load it into the lesson 
+		$.getScript("lessons/" + this.id,function(data) {
+			var json_data = JSON.parse(data); // lessons must be in valid JSON format
 
-		var lesson = new Lesson(json_data);
-		var counter = 0;
+			var lesson = new Lesson(json_data);
+			var counter = 0;
 
-		// insert initial command and explanation
-		$("#lesson-box").attr('placeholder', lesson.short_cuts[0].command);
-		$("#lesson-explanation").text(lesson.short_cuts[0].explanation);
+			// insert initial command and explanation
+			$("#lesson-box").attr('placeholder', lesson.short_cuts[0].command);
+			$("#lesson-explanation").text(lesson.short_cuts[0].explanation);
 
-		$("#lesson-box").on("input", function() {
+			$("#lesson-box").on("input", function() {
 
-			// if correct iterate to the next value
-			if(lesson.short_cuts[counter].command == $("#lesson-box").val()) {
-				counter++;
-			}
+				// if correct iterate to the next value
+				if(lesson.short_cuts[counter].command == $("#lesson-box").val()) {
+					counter++;
+				}
 
-			// clear the box afterwards
-			setTimeout(function() {
-				$("#lesson-box").val("");
-			}, 450);
+				// clear the box afterwards
+				setTimeout(function() {
+					$("#lesson-box").val("");
+				}, 450);
 
-			// loop counter back around
-			// because Objects are unordered associative arrays,
-			// we have to get the length by taking the array of keys for the object
-			if(counter > Object.keys(lesson.short_cuts).length - 1) {
-				counter = 0;
-			}
+				// loop counter back around
+				// because Objects are unordered associative arrays,
+				// we have to get the length by taking the array of keys for the object
+				if(counter > Object.keys(lesson.short_cuts).length - 1) {
+					counter = 0;
+				}
 
-			// put new character in the box
-			$("#lesson-box").attr("placeholder", lesson.short_cuts[counter].command);
-			$("#lesson-explanation").text(lesson.short_cuts[counter].explanation);
+				// put new character in the box
+				$("#lesson-box").attr("placeholder", lesson.short_cuts[counter].command);
+				$("#lesson-explanation").text(lesson.short_cuts[counter].explanation);
+			});
 		});
 	});
 });
